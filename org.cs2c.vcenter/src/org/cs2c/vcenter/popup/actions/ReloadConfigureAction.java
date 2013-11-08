63e4d26fbb0f1d37141775fe32974bcd24de82fd
welcome <welcome@welcome-PC>
2013-11-08 14:21:35 +0800

    op nginx

--- org.cs2c.vcenter/src/org/cs2c/vcenter/popup/actions/ReloadConfigureAction.java
+++ org.cs2c.vcenter/src/org/cs2c/vcenter/popup/actions/ReloadConfigureAction.java
@@ -3,8 +3,13 @@
  */
 package org.cs2c.vcenter.popup.actions;
 
+import org.cs2c.nginlib.AuthInfo;
+import org.cs2c.nginlib.MiddlewareFactory;
+import org.cs2c.nginlib.RemoteException;
+import org.cs2c.nginlib.ctl.Controller;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
@@ -13,7 +18,7 @@ import org.eclipse.ui.IWorkbenchPart;
  *
  */
 public class ReloadConfigureAction implements IObjectActionDelegate {
-
+	private Shell shell;
 	/**
 	 * 
 	 */
@@ -27,9 +32,36 @@ public class ReloadConfigureAction implements IObjectActionDelegate {
 	@Override
 	public void run(IAction action) {
 		// TODO Auto-generated method stub
+		try {
+			ReloadConf();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	private void ReloadConf() throws RemoteException{
+		AuthInfo authInfo=MiddlewareFactory.newAuthInfo();
+
+		authInfo.setHost("10.1.50.4");
+		authInfo.setUsername("root");
+		authInfo.setPassword("cs2csolutions");
+		
+		MiddlewareFactory instance1 = null;
+		instance1 = MiddlewareFactory.getInstance(authInfo, "/usr/local/nginx/");
+
+		Controller reccontro=instance1.getController();
+		if(reccontro==null)
+		{
+			throw new RemoteException("reccontro=null");
+		}
+		else
+		{
+			reccontro.reload();
+		}
 
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
 	 */
@@ -45,7 +77,7 @@ public class ReloadConfigureAction implements IObjectActionDelegate {
 	@Override
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 		// TODO Auto-generated method stub
-
+		shell = targetPart.getSite().getShell();
 	}
 
 }
