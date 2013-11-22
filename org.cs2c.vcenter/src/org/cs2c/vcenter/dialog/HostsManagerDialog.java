a90a54ffe58333c2e8a997594a2423693ef438b6
liuqin2013 <qin.liu@bj.cs2c.com.cn>
2013-11-22 14:07:35 +0800

    2013-11-22 used to test

--- org.cs2c.vcenter/src/org/cs2c/vcenter/dialog/HostsManagerDialog.java
+++ org.cs2c.vcenter/src/org/cs2c/vcenter/dialog/HostsManagerDialog.java
@@ -20,9 +20,17 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.PaintEvent;
 
 public class HostsManagerDialog extends Dialog {
-	DOMParser hostData=null;
+	DOMParser hostXml = null;
+	List list = null;
+	GridData gd_list;
+	Button btnNewButton = null;
+	Button btnNewButton_1 = null;
+	Button btnNewButton_2 = null;
+
 	/**
 	 * Create the dialog.
 	 * 
@@ -30,9 +38,8 @@ public class HostsManagerDialog extends Dialog {
 	 */
 	public HostsManagerDialog(Shell parentShell) {
 		super(parentShell);
-		hostData=new DOMParser();
-		
-		
+		hostXml = new DOMParser("d:/host.xml");
+
 	}
 
 	/**
@@ -43,92 +50,116 @@ public class HostsManagerDialog extends Dialog {
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
+		container.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+
+			}
+		});
 		GridLayout gridLayout = (GridLayout) container.getLayout();
 		gridLayout.numColumns = 8;
 
-		List list = new List(container, SWT.BORDER);
+		list = new List(container, SWT.BORDER);
+		btnNewButton = new Button(container, SWT.NONE);
+		btnNewButton_1 = new Button(container, SWT.NONE);
+		btnNewButton_2 = new Button(container, SWT.NONE);
+			
+		getHostsInfoFromXml();
 
-
-		for(int i=0;i<hostData.getMainHostInfo("d:/host.xml").size();i++)
-		{	
-				
-			list.add(hostData.getMainHostInfo("d:/host.xml").get(i));
-		}
-		
-		
-		GridData gd_list = new GridData(SWT.LEFT, SWT.CENTER, false, false, 5,
-				5);
+		gd_list = new GridData(SWT.LEFT, SWT.CENTER, false, false, 5, 5);
 		gd_list.heightHint = 229;
 		gd_list.widthHint = 282;
 		list.setLayoutData(gd_list);
-		
-		new Label(container, SWT.NONE);
-		
-				Button btnNewButton = new Button(container, SWT.NONE);
-				btnNewButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseDown(MouseEvent e) {
-						//list.getItems()
-						HostsEditDialog addhostDialog=new HostsEditDialog(this.getShell(),1);
 
-						addhostDialog.open();
-					}
+		new Label(container, SWT.NONE);
 
-					private Shell getShell() {
-						// TODO Auto-generated method stub
-						return null;
-					}
-				});
-				btnNewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
-						false, 2, 1));
-				btnNewButton.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-					}
-				});
-				btnNewButton.setText("Add");
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
-				new Label(container, SWT.NONE);
-		
-				Button btnNewButton_1 = new Button(container, SWT.NONE);
-				btnNewButton_1.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseDown(MouseEvent e) {
-						HostsEditDialog edithostDialog=new HostsEditDialog(this.getShell(),2,list.getItems());
-						
-						edithostDialog.open();//edithostDialog.initEditDialog();
-						
-						
-					}
+	
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// list.getItems()
+				HostsEditDialog addhostDialog = new HostsEditDialog(this
+						.getShell(), "", hostXml);
 
-					private Shell getShell() {
-						// TODO Auto-generated method stub
-						return null;
-					}
-				});
-				btnNewButton_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
-						false, 2, 1));
-				btnNewButton_1.setText("Edit");
-		new Label(container, SWT.NONE);
-		
-				Button btnNewButton_2 = new Button(container, SWT.NONE);
-				btnNewButton_2.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
-				btnNewButton_2.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseDown(MouseEvent e) {
-						MessageBox mb = new MessageBox(getShell(), SWT.OK);
-						mb.setMessage("Hello world!");
-						mb.setText("Demo");
-						mb.open();
-					}
-				});
-				btnNewButton_2.setText("Delete");
+				if (addhostDialog.open() == OK)	
+				{
+					getHostsInfoFromXml();
+				}
 
+			}
+
+			private Shell getShell() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		btnNewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 2, 1));
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setText("Add");
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+
+		btnNewButton_1.setEnabled(false);
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				HostsEditDialog edithostDialog = new HostsEditDialog(this
+						.getShell(), list.getSelection()[0], hostXml);
+
+				if (edithostDialog.open() == OK)
+				{
+					getHostsInfoFromXml();
+				}
+
+			}
+
+			private Shell getShell() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		btnNewButton_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 2, 1));
+		btnNewButton_1.setText("Edit");
+		new Label(container, SWT.NONE);
+
+		btnNewButton_2.setEnabled(false); 
+		btnNewButton_2.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 2, 1));
+		btnNewButton_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				hostXml.deleteHostInfo(list.getSelection()[0]);		
+				hostXml.saveXml(hostXml.getDocument(), "d:/host.xml");
+				
+				getHostsInfoFromXml();
+
+			}
+		});
+		btnNewButton_2.setText("Delete");
+
+		list.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				btnNewButton_1.setEnabled(true);
+				btnNewButton_2.setEnabled(true);
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				btnNewButton_1.setEnabled(true);
+				btnNewButton_2.setEnabled(true);
+			}
+		});
 		return container;
 	}
 
@@ -149,5 +180,23 @@ public class HostsManagerDialog extends Dialog {
 		return new Point(450, 300);
 	}
 
-
+	public void getHostsInfoFromXml() {
+		list.removeAll();
+		for (int i = 0; i < hostXml.getMainHostInfo().size(); i++) {
+			list.add(hostXml.getMainHostInfo().get(i));
+		}
+		list.setFocus();
+		if(list.getItemCount()!=0) 
+		{
+			list.setSelection(0);
+			btnNewButton_1.setEnabled(true);
+			btnNewButton_2.setEnabled(true);
+		}
+		else
+		{
+			btnNewButton_1.setEnabled(false);
+			btnNewButton_2.setEnabled(false);
+		}
+		
+	}
 }
--- org.cs2c.vcenter/src/org/cs2c/vcenter/metadata/DOMParser.java
+++ org.cs2c.vcenter/src/org/cs2c/vcenter/metadata/DOMParser.java
@@ -1,219 +1,222 @@
 package org.cs2c.vcenter.metadata;
 
-
-import java.io.File; 
-import java.io.IOException; 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder; 
-import javax.xml.parsers.DocumentBuilderFactory; 
-import javax.xml.parsers.ParserConfigurationException; 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.w3c.dom.Document; 
-import org.w3c.dom.Element; 
-import org.w3c.dom.Node; 
-import org.w3c.dom.NodeList; 
-import org.xml.sax.SAXException; 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+public class DOMParser {
+	DocumentBuilderFactory builderFactory = DocumentBuilderFactory
+			.newInstance();
+	Document document = null;
+	String hostName = null;
+	String userName = null;
+	String passWord = null;
 
-
-public class DOMParser { 
-	   DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance(); 
-	   String hostName=null;
-	   String userName=null;
-	   String passWord=null;
-	  
-	   //Load and parse XML file into DOM 
-	   public Document parse(String filePath) { 
-	      Document document = null; 
-	      try { 
-	         //DOM parser instance 
-	         DocumentBuilder builder = builderFactory.newDocumentBuilder(); 
-	         //parse an XML file into a DOM tree 
-	         document = builder.parse(new File(filePath));
-	        
-	      } catch (ParserConfigurationException e) { 
-	         e.printStackTrace();  
-	      } catch (SAXException e) { 
-	         e.printStackTrace(); 
-	      } catch (IOException e) { 
-	         e.printStackTrace(); 
-	      } 
-	      return document; 
-	   } 
-	   
-	   public List<String> getMainHostInfo(String xmlFile)
-	   {
-		   List<String> mainHostInfo=null;
-		   mainHostInfo=new ArrayList<String>(0);
-		   Document document = this.parse(xmlFile); 
-		   Element rootElement = document.getDocumentElement(); 
-	       NodeList nodeList = rootElement.getElementsByTagName("host");
-	       //System.out.println(rootElement.getElementsByTagName("host").getLength());
-	       for (int i=0; i < nodeList.getLength(); i++) 
-	       { 
-	            Element element = (Element)nodeList.item(i); 
-	            //System.out.println(element.getAttribute("name"));
-	            mainHostInfo.add(element.getAttribute("middleware")+"-"+element.getAttribute("name"));
-	       } 
-		   return mainHostInfo;
-	   }
-	   public List<String> getHostInfo(String xmlFile,String mainHostInfo)
-	   {
-		    List<String> hostInfo= null;
-		    hostInfo=new ArrayList<String>(0);
-		   String host[]=mainHostInfo.split("-", 2);
-		   System.out.println(host[0]);
-		   System.out.println(host[1]);
-		   Document document = this.parse(xmlFile); 
-		   Element rootElement = document.getDocumentElement(); 
-	       NodeList nodeList = rootElement.getElementsByTagName("host");
-	       System.out.println(rootElement.getElementsByTagName("host").getLength());
-	       for (int i=0; i < nodeList.getLength(); i++) 
-	       { 
-	    	   Element element = (Element)nodeList.item(i); 
-	           // System.out.println(element.toString());
-	            System.out.println(element.getAttribute("middleware"));
-	            if((element.getAttribute("middleware").compareTo(host[0])==0)&&(element.getAttribute("name").compareTo(host[1])==0))
-	            {
-	            	hostInfo.add(element.getAttribute("name"));
-	            	hostInfo.add(element.getAttribute("username"));
-	            	hostInfo.add(element.getAttribute("password"));
-	            	hostInfo.add(element.getAttribute("middleware"));
-	            	hostInfo.add(element.getAttribute("home"));
-	            	hostInfo.add(element.getAttribute("stapath"));
-	            	hostInfo.add(element.getAttribute("musername"));
-	            	hostInfo.add(element.getAttribute("mpassword"));
-	            	return hostInfo;
-	    
-	            	 }
-	       } 
-		   return null;
-	   }
-		
-	   public boolean deleteHostInfo(String xmlFile,String mainHostInfo)
-	   {
-		   String host[]=mainHostInfo.split("-", 2);
-		   System.out.println(host[0]);
-		   System.out.println(host[1]);
-		   Document document = this.parse(xmlFile); 
-		   Element rootElement = document.getDocumentElement(); 
-	       NodeList nodeList = rootElement.getElementsByTagName("host");
-	       System.out.println(rootElement.getElementsByTagName("host").getLength());
-	       for (int i=0; i < nodeList.getLength(); i++) 
-	       { 
-	            Element element = (Element)nodeList.item(i); 
-	            if((element.getAttribute("middleware").compareTo(host[0])==0)&&(element.getAttribute("name").compareTo(host[1])==0))
-	            {
-	            
-	            	if(nodeList.item(i).getParentNode().removeChild(nodeList.item(i))!=null)
-	            	{
-	            		//delete the blank line
-	            		System.out.println(rootElement.getElementsByTagName("host").getLength());
-	            		saveXml(document,xmlFile);
-	            			return true;
-	            	}
-	            }
-	       } 
-	       System.out.println("112");
-	       return false;
-	   }
-	   public boolean saveHostInfo(String xmlFile,List<String> hostInfo)
-	   {
-		  
-		   
-		   Document document = this.parse(xmlFile); 
-		   Element rootElement = document.getDocumentElement(); 
-	       NodeList nodeList = rootElement.getElementsByTagName("host");
-	       System.out.println(rootElement.getElementsByTagName("host").getLength());
-	       
-	       for (int i=0; i < nodeList.getLength(); i++) 
-	       { 
-	    	   
-	            Element element = (Element)nodeList.item(i); 
-	            if((element.getAttribute("middleware").compareTo(hostInfo.get(3))==0)&&(element.getAttribute("name").compareTo(hostInfo.get(0))==0))
-	            {
-	            	element.setAttribute("name", hostInfo.get(0));
-	            	element.setAttribute("username", hostInfo.get(1));
-	            	element.setAttribute("password", hostInfo.get(2));
-	            	element.setAttribute("middleware", hostInfo.get(3));
-	            	element.setAttribute("home", hostInfo.get(4));
-	            	element.setAttribute("stapath", hostInfo.get(5));
-	            	element.setAttribute("musername", hostInfo.get(6));
-	            	element.setAttribute("upassword", hostInfo.get(7));
-	            	saveXml(document,xmlFile);
-	            	return true;
-	            }
-	            
-	       } 
-	       System.out.println("111");
-	       Element theHost=null;
-	       theHost=document.createElement("host");
-	       theHost.setAttribute("name", hostInfo.get(0));
-	       theHost.setAttribute("username", hostInfo.get(1));
-	       theHost.setAttribute("password", hostInfo.get(2));
-	       theHost.setAttribute("middleware", hostInfo.get(3));
-	       theHost.setAttribute("home", hostInfo.get(4));
-	       theHost.setAttribute("stapath", hostInfo.get(5));
-	       theHost.setAttribute("musername", hostInfo.get(6));
-	       theHost.setAttribute("upassword", hostInfo.get(7));
-	       rootElement.appendChild(theHost);
-       	   if(saveXml(document,xmlFile))
-       		   return true;
-       	   else
-       		   return false;
-	           
-	       
-	   }
-	   
-	   private boolean saveXml(Document document,String filename) {
-		// TODO Auto-generated method stub
-		   boolean flag = true; 
-		      try 
-		       { 
-		            /** 将document中的内容写入文件中   */ 
-		             TransformerFactory tFactory = TransformerFactory.newInstance();    
-		             Transformer transformer = tFactory.newTransformer();  
-		            /** 编码 */ 
-		            //transformer.setOutputProperty(OutputKeys.ENCODING, "GB2312"); 
-		             DOMSource source = new DOMSource(document);  
-		             StreamResult result = new StreamResult(new File(filename));    
-		             transformer.transform(source, result);  
-		         }catch(Exception ex) 
-		         { 
-		             flag = false; 
-		             ex.printStackTrace(); 
-		         } 
-		        return flag;       
+	public DOMParser(String filePath) {
+		document = parseXml(filePath);
 	}
 
-	public static void main(String[] args) { 
-	         DOMParser parser = new DOMParser(); 
-	       /*  if(parser.deleteHostInfo("host.xml","Nginx-10.1.50.4"))
-	        	 System.out.println("ok");
-	      */
-	        /*
+	public  Document getDocument()
+	{
+		return document;
+	}
+	// Load and parse XML file into DOM
+	public Document parseXml(String filePath) {
+		Document document = null;
+		try {
+			// DOM parser instance
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			// parse an XML file into a DOM tree
+			document = builder.parse(new File(filePath));
 
-			 List<String> hostInfo= null;
-			 hostInfo=new ArrayList<String>(0);
-			 hostInfo.add("10.1.50.4");
-			 hostInfo.add("112");
-			 hostInfo.add("111");
-			 hostInfo.add("Nginx");
-			 hostInfo.add("111");
-			 hostInfo.add("111");
-			 hostInfo.add("111");
-			 hostInfo.add("111");
-			 System.out.println(hostInfo);
-	         if(parser.saveHostInfo("host.xml", hostInfo)==true)
-	        	 System.out.println("ok");
-	        	 */
-	         System.out.println(parser.getMainHostInfo("d:/host.xml"));
-	   } 
-	 } 
-	    
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+
+	public List<String> getMainHostInfo() {
+		List<String> mainHostInfo = null;
+		mainHostInfo = new ArrayList<String>(0);
+
+		Element rootElement = document.getDocumentElement();
+		NodeList nodeList = rootElement.getElementsByTagName("host");
+		// System.out.println(rootElement.getElementsByTagName("host").getLength());
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element) nodeList.item(i);
+			// System.out.println(element.getAttribute("name"));
+			mainHostInfo.add(element.getAttribute("middleware") + "-"
+					+ element.getAttribute("name"));
+		}
+		return mainHostInfo;
+	}
+
+	public HostInfo getHostInfo(String mainHostInfo) {
+		HostInfo hostInfo = new HostInfo();
+
+		String host[] = mainHostInfo.split("-", 2);
+		System.out.println(host[0]);
+		System.out.println(host[1]);
+
+		Element rootElement = document.getDocumentElement();
+		NodeList nodeList = rootElement.getElementsByTagName("host");
+		System.out
+				.println(rootElement.getElementsByTagName("host").getLength());
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element) nodeList.item(i);
+			// System.out.println(element.toString());
+			System.out.println(element.getAttribute("middleware"));
+			if ((element.getAttribute("middleware").compareTo(host[0]) == 0)
+					&& (element.getAttribute("name").compareTo(host[1]) == 0)) {
+				hostInfo.setHostName(element.getAttribute("name"));
+				hostInfo.setUserName(element.getAttribute("username"));
+				hostInfo.setPassWord(element.getAttribute("password"));
+				hostInfo.setMiddleware(element.getAttribute("middleware"));
+				hostInfo.setHome(element.getAttribute("home"));
+				hostInfo.setStatusPath(element.getAttribute("stapath"));
+				hostInfo.setManagerUserName(element.getAttribute("musername"));
+				hostInfo.setManagerPassWord(element.getAttribute("mpassword"));
+				return hostInfo;
+
+			}
+		}
+		return null;
+	}
+
+	public boolean deleteHostInfo(String mainHostInfo) {
+		String host[] = mainHostInfo.split("-", 2);
+		System.out.println(host[0]);
+		System.out.println(host[1]);
+
+		Element rootElement = document.getDocumentElement();
+		NodeList nodeList = rootElement.getElementsByTagName("host");
+		System.out
+				.println(rootElement.getElementsByTagName("host").getLength());
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element) nodeList.item(i);
+			if ((element.getAttribute("middleware").compareTo(host[0]) == 0)
+					&& (element.getAttribute("name").compareTo(host[1]) == 0)) {
+
+				if (nodeList.item(i).getParentNode()
+						.removeChild(nodeList.item(i)) != null) {
+					// delete the blank line
+					System.out.println(rootElement.getElementsByTagName("host")
+							.getLength());
+
+					return true;
+				}
+			}
+		}
+		System.out.println("112");
+		return false;
+	}
+
+	public boolean saveHostInfo(String xmlFile, HostInfo hostInfo) {
+
+		Element rootElement = document.getDocumentElement();
+		NodeList nodeList = rootElement.getElementsByTagName("host");
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+
+			Element element = (Element) nodeList.item(i);
+			if ((element.getAttribute("middleware").compareTo(
+					hostInfo.getMiddleware()) == 0)
+					&& (element.getAttribute("name").compareTo(
+							hostInfo.getHostName()) == 0)) {
+				element.setAttribute("name", hostInfo.getHostName());
+				element.setAttribute("username", hostInfo.getUserName());
+				element.setAttribute("password", hostInfo.getPassWord());
+				element.setAttribute("middleware", hostInfo.getMiddleware());
+				element.setAttribute("home", hostInfo.getHome());
+				element.setAttribute("stapath", hostInfo.getStatusPath());
+				element.setAttribute("musername", hostInfo.getManagerUserName());
+				element.setAttribute("mpassword", hostInfo.getManagerPassWord());
+				saveXml(document, xmlFile);
+				return true;
+			}
+
+		}
+
+		Element theHost = null;
+		theHost = document.createElement("host");
+		theHost.setAttribute("name", hostInfo.getHostName());
+		theHost.setAttribute("username", hostInfo.getUserName());
+		theHost.setAttribute("password", hostInfo.getPassWord());
+		theHost.setAttribute("middleware", hostInfo.getMiddleware());
+		theHost.setAttribute("home", hostInfo.getHome());
+		theHost.setAttribute("stapath", hostInfo.getStatusPath());
+		theHost.setAttribute("musername", hostInfo.getManagerUserName());
+		theHost.setAttribute("mpassword", hostInfo.getManagerPassWord());
+
+		rootElement.appendChild(theHost);
+		if (saveXml(document, xmlFile)) {
+			// System.out.println(hostXml.getMainHostInfo());
+			return true;
+		} else
+			return false;
+
+	}
+
+	public boolean saveXml(Document document, String filename) {
+		// TODO Auto-generated method stub
+		boolean flag = true;
+		try {
+			/** 将document中的内容写入文件中 */
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer transformer = tFactory.newTransformer();
+			/** 编码 */
+			// transformer.setOutputProperty(OutputKeys.ENCODING, "GB2312");
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(filename));
+			transformer.transform(source, result);
+
+		} catch (Exception ex) {
+			flag = false;
+			ex.printStackTrace();
+		}
+		return flag;
+	}
+
+	public static void main(String[] args) {
+		DOMParser parser = new DOMParser("d:/host.xml");
+		/*
+		 * if(parser.deleteHostInfo("host.xml","Nginx-10.1.50.4"))
+		 * System.out.println("ok");
+		 */
+
+		HostInfo hostInfo = null;
+		hostInfo = new HostInfo();
+		hostInfo.setManagerPassWord("10.1.50.5");
+		hostInfo.setHome("112");
+		hostInfo.setManagerPassWord("111");
+		hostInfo.setManagerUserName("Nginx");
+		hostInfo.setPassWord("111");
+		hostInfo.setUserName("111");
+		hostInfo.setMiddleware("111");
+		hostInfo.setHostName("23");
+
+		if (parser.saveHostInfo("d:/host.xml", hostInfo) == true)
+			System.out.println("ok");
+
+		System.out.println(parser.getMainHostInfo());
+	}
+}
